33cc70ef9cb613e8f8aa783793abfb76a1d550f2
Audrey Mengue <massoumeharmonie@gmail.com>
2022-06-04 06:59:33 +0000

    Testing gh-pages branch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
         <h3>Hey, CI/CD here with Azure devOps pipelines, we tried and it worked perfectly today too.</h3>
         <h3>Hey, CI/CD Demo 1 was a success</h3>
        
+        <h3>Hey, CI/CD Demo 1 was a success</h3>
+       
       </section>
       
     </main>
